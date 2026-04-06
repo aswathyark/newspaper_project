1bd58631916e38e10bc9c4ce8f68fe8affbe7b38
aswathy <aswathyar2701@gmail.com>
2026-04-06 11:33:20 +0530

    Ready for deployment

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^luy6u=g4)(&f0&=+(d^z$2ud6m823w-7oo2_8q05r+&p8-3+9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+      'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
 
     'django.middleware.locale.LocaleMiddleware',
@@ -133,3 +134,4 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
